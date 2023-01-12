e35ca0f8c88b1142587aa1d2b955e0015f98dc89
InSIkHwang <85327744+InSIkHwang@users.noreply.github.com>
2023-01-12 21:05:34 +0900

    Add files via upload

---
+++ new.js
@@ -0,0 +1,28 @@
+const calculator = {
+    add: function(a, b){
+        console.log(a + b);
+    },
+    minus: function(a, b){
+        console.log(a - b);
+    },
+    power: function(a, b){
+        console.log(a ** b);
+    },
+    div: function(a, b){
+        console.log(a / b);
+    },
+    mul: function(a, b){
+        console.log(a * b);
+    },
+
+};
+
+calculator.add(5, 5);
+
+calculator.minus(5, 5);
+
+calculator.power(5, 2);
+
+calculator.div(5, 5);
+
+calculator.mul(5, 5);
